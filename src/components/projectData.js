574061d3d5760d282bbe3355f397307395e93fe7
rowlandwilliams <Rowland@Matthews-MacBook-Air.local>
2020-10-27 13:06:23 +0000

    added site data files

---
+++ src/components/projectData.js
@@ -0,0 +1,20 @@
+export const projectData = [
+    {
+        img: './img/carbon.png',
+        title: 'Carbon Explorer',
+        desc: 'Design, Development',
+        descSub: 'A visual...'
+    },
+    {
+        img: './img/carbon.png',
+        title: 'Carbon Explorer',
+        desc: 'Design, Development',
+        descSub: 'A visual...'
+    },
+    {
+        img: './img/carbon.png',
+        title: 'Carbon Explorer',
+        desc: 'Design, Development',
+        descSub: 'A visual...'
+    }
+]
