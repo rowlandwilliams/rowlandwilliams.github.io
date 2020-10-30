d8a4460e8ff559d0fd42d231220d60285031c3d5
rowlandwilliams <Rowland@Matthews-MacBook-Air.local>
2020-10-30 11:03:23 +0000

    fixed gif bug

--- src/components/projectData.js
+++ src/components/projectData.js
@@ -10,7 +10,7 @@ export const projectData = [
     },
     {
         name: 'p2',
-        img: '/img/widget.gif',
+        img: 'img/widget.gif',
         title: 'Reporting Daily CO<sub>2</sub>',
         desc: 'Data Research, Development',
         descSub: 'Ongoing project with Matthew Shribman and Cambridge Zero. A widget showing daily Carbon Dioxide levels to display on major news websites. Front-end development.',
@@ -18,7 +18,7 @@ export const projectData = [
     },
     {
         name: 'p3',
-        img: '/img/matopiba.gif',
+        img: 'img/matopiba.gif',
         url: 'https://medium.com/trase/matopiba-brazils-soy-frontier-9ad4cc6fe2d9',
         title: 'MATOPIBA: Brazil\'s new soy frontier',
         desc: 'Analysis, Visualization, Copy',
